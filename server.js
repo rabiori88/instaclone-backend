4b837d6f55e4b3fd8cc5c61a833b08d26af2ebc2
rabiori <ctangtangbbang@gmail.com>
2021-02-14 01:15:44 +0900

    Prisma CRUD 테스트 완

--- server.js
+++ server.js
@@ -1,19 +1,50 @@
-//const { ApolloServer, gql } = require('apollo-server');
 
 import {ApolloServer, gql} from "apollo-server";
 
+import pkg from '@prisma/client';
+const { PrismaClient } = pkg;
+
+const client = new PrismaClient();
+
 // The GraphQL schema
 const typeDefs = gql`
+  type Movie {
+   id: Int!
+   title: String!
+   year: Int
+   genre: String
+   createdAt: String!
+   updatedAt: String!
+  }
   type Query {
-    "A simple type for getting started!"
-    hello: String
+    movies: [Movie]
+    movie(id:Int!): Movie
+  }
+  type Mutation {
+    createMovie(title: String!, year:Int!, genre: String): Movie
+    deleteMovie(id: Int!): Movie
+    updateMovie(id: Int!, year: Int!): Movie
   }
 `;
 
 // A map of functions which return data for the schema.
 const resolvers = {
   Query: {
-    hello: () => 'TEsdfsfST',
+    movies: () => client.movie.findMany(), 
+    movie: (_, {id}) => client.movie.findUnique({ where: {id }}),
+  },
+  Mutation: {
+    createMovie: (_, {title, year, genre}) => 
+      client.movie.create({ 
+        data: {
+          title,
+          year,
+          genre,
+        },
+      }),    
+    
+    deleteMovie: (_ ,{id}) => client.movie.delete({ where: { id } } ),
+    updateMovie: (_ ,{id, year}) => client.movie.update({ where: { id }, data: {year} } ),
   },
 };
 
