4d268c40057518940e968455e3c18a6d26469747
rabiori <ctangtangbbang@gmail.com>
2021-02-16 22:13:27 +0900

    Movie 모듈화

--- server.js
+++ server.js
@@ -1,56 +1,11 @@
 
-import {ApolloServer, gql} from "apollo-server";
+import {ApolloServer} from "apollo-server";
+import schema from "./schema.js"
 
-import pkg from '@prisma/client';
-const { PrismaClient } = pkg;
 
-const client = new PrismaClient();
-
-// The GraphQL schema
-const typeDefs = gql`
-  type Movie {
-   id: Int!
-   title: String!
-   year: Int
-   genre: String
-   createdAt: String!
-   updatedAt: String!
-  }
-  type Query {
-    movies: [Movie]
-    movie(id:Int!): Movie
-  }
-  type Mutation {
-    createMovie(title: String!, year:Int!, genre: String): Movie
-    deleteMovie(id: Int!): Movie
-    updateMovie(id: Int!, year: Int!): Movie
-  }
-`;
-
-// A map of functions which return data for the schema.
-const resolvers = {
-  Query: {
-    movies: () => client.movie.findMany(), 
-    movie: (_, {id}) => client.movie.findUnique({ where: {id }}),
-  },
-  Mutation: {
-    createMovie: (_, {title, year, genre}) => 
-      client.movie.create({ 
-        data: {
-          title,
-          year,
-          genre,
-        },
-      }),    
-    
-    deleteMovie: (_ ,{id}) => client.movie.delete({ where: { id } } ),
-    updateMovie: (_ ,{id, year}) => client.movie.update({ where: { id }, data: {year} } ),
-  },
-};
 
 const server = new ApolloServer({
-  typeDefs,
-  resolvers,
+  schema,
 });
 
 server.listen().then(({ url }) => {
