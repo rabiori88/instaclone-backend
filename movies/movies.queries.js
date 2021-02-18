62e72fe5210b36a7bcc69fbff8bf7c912e3d985a
rabiori <ctangtangbbang@gmail.com>
2021-02-19 00:04:57 +0900

    create account
    중복체크

--- movies/movies.queries.js
+++
@@ -1,9 +0,0 @@
-import client from "../client.js";
-// A map of functions which return data for the schema.
-export default {
-    Query: {
-      movies: () => client.movie.findMany(), 
-      movie: (_, {id}) => client.movie.findUnique({ where: {id }}),
-    },
-  
-};
