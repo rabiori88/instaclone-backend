0065e7d6b29244b1f189b530e5579cec838c34ef
rabiori <ctangtangbbang@gmail.com>
2021-03-01 10:46:00 +0900

    3.7 Prisma client part Two

--- server.js
+++ server.js
@@ -22,23 +22,20 @@ const typeDefs = gql`
 
     type Query {
         movies: [Movie]
-        movie: Movie
+        movie(id: Int!): Movie
     }
 
     type Mutation {
         createMovie(title: String!, year: Int!, genre: String!): Movie
-        deleteMovie(title: String!): Boolean
+        deleteMovie(id: Int!): Movie
+        updateMovie(id: Int!, year: Int!): Movie
     }
 `;
 
 const resolvers = {
     Query: {
         movies: () =>  client.movie.findMany(),
-        movie: () => 
-        {
-            ({title: "Hello", year: 2021}),
-            console.log("movie Test");
-        }
+        movie: (_, {id}) => client.movie.findUnique({where : {id}}), 
       
     },
     Mutation: {
@@ -50,11 +47,11 @@ const resolvers = {
                     genre
                 },     
             }), 
+           
         
-        deleteMovie: (_, {title}) => {
-            console.log(title);
-            return true
-        },
+        
+        deleteMovie: (_, {id}) => client.movie.delete({where : {id}}),
+        updateMovie: (_, {id, year}) => client.movie.update({where : {id}, data: {year}}),
     },
   };
 
