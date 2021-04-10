8a56f2aacea8ddeee98b2a2e01d7437caddfe822
rabiori <ctangtangbbang@gmail.com>
2021-04-10 23:06:42 +0900

    4.12 Protecting Resolvers part Two

--- users/edtiProfile/editProfile.resolvers.js
+++ users/edtiProfile/editProfile.resolvers.js
@@ -1,56 +1,57 @@
 import client from "../../client";
 import bcrypt from "bcrypt";
+import { protectResolver } from "../users.utils";
 
 export default {
     Mutation: {
-        edtiProfile: async (
-            _ , 
-            {             
-                firstName,
-                lastName,
-                userName,
-                email,
-                password: newPassword,
-                                
-            },
-            { loggedInUser, protectResolver }
-            )=> {
-               protectResolver(loggedInUser);
-
-               console.log(loggedInUser);
-                let uglyPassword = null;
-                if(newPassword) {
-                    uglyPassword = await bcrypt.hash(newPassword, 10);
-                }
-
-                const ok =  await client.user.update({
-                    where :{
-                     id: loggedInUser.id,
-                    }, 
-                    data:{
-                        firstName, 
-                        lastName, 
-                        userName, 
-                        email, 
-                        ...(uglyPassword && { password: uglyPassword }),
-                    },
-                });
-
-                 if (ok) {
-                    return {
-                        ok: true,
-                        error: "success",
+        edtiProfile: protectResolver(
+            async (
+                _ , 
+                {             
+                    firstName,
+                    lastName,
+                    userName,
+                    email,
+                    password: newPassword,
+                                    
+                },
+                { loggedInUser }
+                )=> {                     
+                  
+                    let uglyPassword = null;
+                    if(newPassword) {
+                        uglyPassword = await bcrypt.hash(newPassword, 10);
                     }
-                    
-                } else 
-                {
-                    return {
-                        ok: false,
-                        error: "Could not update Profile",
+    
+                    const ok =  await client.user.update({
+                        where :{
+                         id: loggedInUser.id,
+                        }, 
+                        data:{
+                            firstName, 
+                            lastName, 
+                            userName, 
+                            email, 
+                            ...(uglyPassword && { password: uglyPassword }),
+                        },
+                    });
+    
+                     if (ok) {
+                        return {
+                            ok: true,
+                            error: "success",
+                        }
                         
+                    } else 
+                    {
+                        return {
+                            ok: false,
+                            error: "Could not update Profile",
+                            
+                        }
                     }
                 }
-            }
+        ) ,
 
            
     },
--- users/users.utils.js
+++ users/users.utils.js
@@ -27,11 +27,13 @@ export const getUser = async(token) => {
 
 };
 
-export const protectResolver =(user) => {
-    if(!user) {
+export const protectResolver = (ourResolver) => (root, args, context, info) => { 
+    if(!context.loggedInUser) {
         return {
             ok: false,
-            error: "You need to login",
-        }
+            error: "Please log in to perform this action.",
+        };
     }
+    return ourResolver(root, args, context, info);
 }
+
